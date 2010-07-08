cae3767e43481d30eab1dadba1d6e55659de2ead
Alexander Kojevnikov <alexander@kojevnikov.com>
2010-07-08 14:32:19 +1000

    Update autogen.sh from Sampala

--- autogen.sh
+++ autogen.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-PKG_NAME="spek"
-REQUIRED_AUTOMAKE_VERSION=1.7
-
-(test -f $srcdir/src/spek.vala \
-  && test -d $srcdir/src) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level spek directory"
-    exit 1
-}
-
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME Git"
-    exit 1
-}
-. gnome-autogen.sh
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdit=.
+(
+  cd "$srcdir" &&
+  AUTOPOINT='intltoolize --automake --copy' autoreconf -fiv
+) || exit
+test -n "$NOCONFIGURE" || "$srcdir/configure" --enable-maintainer-mode "$@"
