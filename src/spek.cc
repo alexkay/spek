71bff01768f1471a3a1e5d0e95f6ea89d278af69
Alexander Kojevnikov <alexander@kojevnikov.com>
2012-08-05 16:28:32 -0700

    Hello, wxWidgets!

--- src/spek.cc
+++ src/spek.cc
@@ -1,6 +1,68 @@
-#include <cstdio>
+#include <wx/wx.h>
 
-int main()
+class MyApp: public wxApp
 {
-    printf("Hello, world!\n");
+    virtual bool OnInit();
+};
+
+class MyFrame: public wxFrame
+{
+public:
+    MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+
+    void OnQuit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+
+private:
+    DECLARE_EVENT_TABLE()
+};
+
+enum
+{
+    ID_Quit = 1,
+    ID_About,
+};
+
+BEGIN_EVENT_TABLE(MyFrame, wxFrame)
+    EVT_MENU(ID_Quit,  MyFrame::OnQuit)
+    EVT_MENU(ID_About, MyFrame::OnAbout)
+END_EVENT_TABLE()
+
+IMPLEMENT_APP(MyApp)
+
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame( wxT("Hello World"), wxPoint(50,50), wxSize(450,340) );
+    frame->Show( true );
+    SetTopWindow( frame );
+    return true;
+}
+
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+       : wxFrame((wxFrame *)NULL, -1, title, pos, size)
+{
+    wxMenu *menuFile = new wxMenu;
+
+    menuFile->Append( ID_About, wxT("&About...") );
+    menuFile->AppendSeparator();
+    menuFile->Append( ID_Quit, wxT("E&xit") );
+
+    wxMenuBar *menuBar = new wxMenuBar;
+    menuBar->Append( menuFile, wxT("&File") );
+
+    SetMenuBar( menuBar );
+
+    CreateStatusBar();
+    SetStatusText( wxT("Welcome to wxWidgets!") );
+}
+
+void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
+{
+    Close( true );
+}
+
+void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
+{
+    wxMessageBox( wxT("This is a wxWidgets' Hello world sample"),
+                  wxT("About Hello World"), wxOK | wxICON_INFORMATION );
 }
