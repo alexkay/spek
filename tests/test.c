a687900e183e5c0191b671e8c893f7153d713864
Alexander Kojevnikov <alexander@kojevnikov.com>
2012-08-27 10:36:34 -0700

    Add tests

---
+++ tests/test.c
@@ -0,0 +1,50 @@
+/* test.c
+ *
+ * Copyright (C) 2012  Alexander Kojevnikov <alexander@kojevnikov.com>
+ *
+ * Spek is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Spek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Spek.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+static int tests_run;
+#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
+#define mu_run_test(test) do { char *message = test(); tests_run++; \
+                               if (message) return message; } while (0)
+
+#include <stdio.h>
+
+static char * test_minunit()
+{
+    mu_assert("Hello, MinUnit", 2 + 2 == 4);
+    return 0;
+}
+
+static char * all_tests()
+{
+    mu_run_test(test_minunit);
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    char *result = all_tests();
+    if (result != 0) {
+        printf("%s\n", result);
+    }
+    else {
+        printf("ALL TESTS PASSED\n");
+    }
+    printf("Tests run: %d\n", tests_run);
+
+    return result != 0;
+}
