b2420a777e48686237fa9338808dab018a5de2d6
Alexander Kojevnikov <alexander@kojevnikov.com>
2012-08-26 19:50:05 -0700

    i18n: Don't ask to translate \n

--- src/spek.cc
+++ src/spek.cc
@@ -87,7 +87,8 @@ bool Spek::OnInit()
     }
     if (parser.Found(wxT("version"))) {
         // TRANSLATORS: first %s is the package name, second %s is the package version.
-        wxPrintf(_("%s version %s\n"), wxT(PACKAGE_NAME), wxT(PACKAGE_VERSION));
+        wxPrintf(_("%s version %s"), wxT(PACKAGE_NAME), wxT(PACKAGE_VERSION));
+        wxPrintf(wxT("\n"));
         this->quit = true;
         return true;
     }
